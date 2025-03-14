7a8e846d21cd4c139ad926f08854c4b6f2ec5e3c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-14 15:21:56 +0530

    Added the loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+        <Image
+        alt='loader'
+        src={"/assets/icons/loader.svg"}
+        height={32}
+        width={32}
+        className='animate-spin'
+        />
+    </div>
+  )
+}
+
+export default Loader
