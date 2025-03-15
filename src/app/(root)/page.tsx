f2b27453788464c87aa5a906f39a995572d26900
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-16 02:38:15 +0530

    Modify the header

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,18 @@
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
   return (
     <main className=" relative flex min-h-screen w-full flex-col items-center gap-5 sm:gap-10">
-      <Header className="sticky left-0 right-0">
-
+      <Header className="sticky left-0 right-0 flex justify-between">
+        <div className="flex items-center gap-2">
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </Header>
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,27 @@
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
-const Header = ({children , className}:HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div>
-      <div className={cn('min-h-[92px] min-w-full flex-nowrap bg-dark-100 flex w-full items-center justify-between gap-2 px-4', className)}>
-            <div>
-            <Link href={"/"}>
-            <Image
-            src={"/assets/icons/logo.svg"}
-            alt='logo with name'
-            width={120}
-            height={32}
-            className='hidden md:block'
-            />
-            <Image
-            src={"/assests/icons/logo-icon.svg"}
-            alt='logo without name'
-            width={32}
-            height={32}
-            className=' md:hidden mr-2'
-            />
-            </Link>
-            </div>
-            <div>
-            {children}
-            </div>
-        </div>
+    <div className={cn("min-h-[92px] min-w-full flex-nowrap bg-dark-100 flex w-full items-center justify-between gap-2 px-4", className)}>
+      <Link href='/' className="md:flex-1">
+        <Image 
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
     </div>
   )
 }
