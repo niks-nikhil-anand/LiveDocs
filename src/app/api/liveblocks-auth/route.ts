15becb0d1fffcaca944c047bce7623b4391e3256
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-15 17:40:19 +0530

    Modify the api liveblocks-auth

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -1,10 +1,26 @@
-import { Liveblocks } from "@liveblocks/node";
+import { liveblocks } from "@/lib/liveblocks";
+import { currentUser } from "@clerk/nextjs/server";
+import { info } from "console";
+import { redirect } from "next/navigation";
 
 
 
 export async function POST(request: Request) {
   // Get the current user from your database
-  const user = __getUserFromDB__(request);
+  const clerkUser = await currentUser();
+
+  if(!clerkUser) redirect("/sign-in");
+
+  const {id, firstName , lastName , emailAddresses , imageUrl } = clerkUser
+  const user = {
+    id, 
+    info:{
+        id , 
+        name : `${firstName} ${lastName}`,
+        email : emailAddresses[0].emailAddress,
+        avatar: imageUrl
+    }
+  }
 
   // Start an auth session inside your endpoint
   const session = liveblocks.prepareSession(
