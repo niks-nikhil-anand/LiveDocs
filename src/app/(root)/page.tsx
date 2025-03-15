3fba2eb0ba3400657aea0a9c6dc004193fd4716c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-16 02:45:17 +0530

    Added the modifcation in the Homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
+  const documents = [];
   return (
     <main className=" relative flex min-h-screen w-full flex-col items-center gap-5 sm:gap-10">
       <Header className="sticky left-0 right-0 flex justify-between">
@@ -14,6 +16,21 @@ const page = () => {
           </SignedIn>
         </div>
       </Header>
+      {documents.length>0 ? (
+        <div>
+
+        </div>
+      ): (
+        <div>
+          <Image
+          src={"/assets/icons/docs.svg"}
+          alt="docs"
+          height={40}
+          width={40}
+          />
+          <AddDocumentBtn/>
+        </div>
+      )}
     </main>
   );
 };
