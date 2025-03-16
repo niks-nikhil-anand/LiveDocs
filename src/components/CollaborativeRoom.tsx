5731f6121b7d0f91cfec6cc7025c91d50cd3d96a
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-16 17:52:40 +0530

    Added the active collabrators

--- src/components/CollaborativeRoom.tsx
+++ src/components/CollaborativeRoom.tsx
@@ -2,12 +2,8 @@
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ActiveCollaborator from "./ActiveCollaborator";
 
 const CollaborativeRoom = () => {
   return (
@@ -16,15 +12,18 @@ const CollaborativeRoom = () => {
         <div className="flex size-full max-h-screen flex-1 flex-col items-center overflow-hidden">
           <Header>
             <div className="flex items-center justify-center gap-2">
-              <SignedOut>
-                <SignInButton />
-              </SignedOut>
-              <SignedIn>
-                <p className="text-white font-bold text-lg cursor-pointer">
-                  Share
-                </p>
-                <UserButton />
-              </SignedIn>
+              <p className="text-white font-bold text-lg cursor-pointer">
+                Share
+              </p>
+              <div className="flex flex-1 w-full justify-end gap-2 sm:gap-3">
+                <ActiveCollaborator />
+                <SignedOut>
+                  <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
             </div>
           </Header>
           <Editor />
