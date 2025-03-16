2ecc96b8f119a0c136728299c6fe6eaa12b366cb
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-17 01:20:52 +0530

    Added the room access , fix the bugs

--- src/lib/actions/room.actions.ts
+++ src/lib/actions/room.actions.ts
@@ -1,35 +1,51 @@
-"use server"
-import {nanoid} from 'nanoid'
-import { liveblocks } from '../liveblocks';
-import { revalidatePath } from 'next/cache';
-import { parseStringify } from '../utils';
+"use server";
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
 
-export const createDocument = async ({userId , email} : CreateDocumentParams) => {
-    const roomId = nanoid();
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "untitled",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ["room:write"],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error while creating Room ${error}`);
+    throw new Error(`Error while creating Room: ${error}`);
+  }
+};
+
+export const getDocument = async ({roomId,userId,}: {roomId: string; userId: string}) => {
     try {
-        const metadata = {
-            creatorId : userId ,
-            email,
-            title : "untitled"
+        const room = await liveblocks.getRoom(roomId)
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId)
+
+        if(!hasAccess){
+            throw new Error('You do not have access to this room')
         }
-
-        const usersAccesses:RoomAccesses = {
-            [email] : ['room:write']
-        }
-
-        const room = await liveblocks.createRoom(roomId, {
-            metadata,
-            usersAccesses,
-            defaultAccesses:  ['room:write']
-        })
-
-        revalidatePath("/")
 
         return parseStringify(room)
-        
     } catch (error) {
-        console.log(`Error while creating Room ${error}`)
-        throw new Error(`Error while creating Room: ${error}`);
-
+        console.log(`Error happend while getting the room ${error}`)
     }
-}
+};
