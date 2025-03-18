fc1c4c665b755cc3b039a4b6d1807eb1978a596c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-18 23:54:39 +0530

    Added mouse down saving title features

--- src/components/CollaborativeRoom.tsx
+++ src/components/CollaborativeRoom.tsx
@@ -4,70 +4,89 @@ import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import ActiveCollaborator from "./ActiveCollaborator";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 
-const CollaborativeRoom = ({roomId , roomMetadata}: CollaborativeRoomProps) => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
+  const currentUserType = "editor";
 
-  const currentUserType = 'editor'
-
-  const [documentTitle , setDocumentTitle] = useState(roomMetadata.title)
-  const [editing, setediting] = useState(false)
-  const [loading, setloading] = useState(false)
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setloading] = useState(false);
 
   const containerRef = useRef<HTMLDivElement>(null);
   const InputRef = useRef<HTMLDivElement>(null);
 
-  const updateTitleHandler = (e:React.KeyboardEvent<HTMLInputElement>) => {
+  const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    
+  };
 
-  }
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setEditing(false);
+      }
+      document.addEventListener("mousedown", handleOutsideClick);
 
+      return () => {
+        document.removeEventListener("mousedown", handleOutsideClick);
+      };
+    };
+  }, []);
 
-  
-
-  
   return (
-
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className="flex size-full max-h-screen flex-1 flex-col items-center overflow-hidden">
           <Header>
-            <div ref={containerRef} className="flex items-center justify-center gap-2">
+            <div
+              ref={containerRef}
+              className="flex items-center justify-center gap-2"
+            >
               {editing && !loading ? (
                 <Input
-                type="text"
-                value={documentTitle}
-                ref={InputRef}
-                placeholder="Enter title"
-                onChange={(e) => setDocumentTitle(e.target.value)}
-                onKeyDown= {updateTitleHandler}
-                disabled = {!editing}
-                className=" min-w-[78px] flex-1 border-none bg-transparent px-0 text-left text-base font-semibold leading-[24px] focus-visible:ring-0 focus-visible:ring-offset-0 disabled:text-black sm:text-xl md:text-center"
+                  type="text"
+                  value={documentTitle}
+                  ref={InputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className=" min-w-[78px] flex-1 border-none bg-transparent px-0 text-left text-base font-semibold leading-[24px] focus-visible:ring-0 focus-visible:ring-offset-0 disabled:text-black sm:text-xl md:text-center"
                 />
               ) : (
                 <>
-                <p className="line-clamp-1 border-dark-400 text-base font-semibold leading-[24px] sm:pl-0 sm:text-xl">{document.title}</p>
+                  <p className="line-clamp-1 border-dark-400 text-base font-semibold leading-[24px] sm:pl-0 sm:text-xl">
+                    {document.title}
+                  </p>
                 </>
               )}
 
-              {currentUserType === 'editor' && !editing && (
+              {currentUserType === "editor" && !editing && (
                 <Image
-                src={"/assets/icons/edit.svg"}
-                alt="edit"
-                width={24}
-                height={24}
-                onClick={() => setediting(true)}
-                className="cursor-pointer"
+                  src={"/assets/icons/edit.svg"}
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="cursor-pointer"
                 />
               )}
 
-              {currentUserType !== 'editor' && !editing && (
-                <p className="rounded-md bg-dark-400/50 px-2 py-0.5 text-xs text-blue-100/50">View only</p>
+              {currentUserType !== "editor" && !editing && (
+                <p className="rounded-md bg-dark-400/50 px-2 py-0.5 text-xs text-blue-100/50">
+                  View only
+                </p>
               )}
 
               {loading && <p className="text-sm text-gray-400">saving..</p>}
-
 
               <div className="flex flex-1 w-full justify-end gap-2 sm:gap-3">
                 <ActiveCollaborator />
