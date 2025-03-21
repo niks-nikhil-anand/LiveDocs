4185859fb7f4508f6227bda0c74be4b7155ff36b
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-21 19:18:33 +0530

    Added button in the share button , improve UI

--- src/components/ShareModal.tsx
+++ src/components/ShareModal.tsx
@@ -1,24 +1,60 @@
-"use client"
-import { useSelf } from '@liveblocks/react/suspense'
-import React, { useState } from 'react'
+"use client";
+import { useSelf } from "@liveblocks/react/suspense";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
-const ShareModal = ({roomId ,collaborators , creatorId , currentUserType} :ShareDocumentDialogProps) => {
+const ShareModal = ({
+  roomId,
+  collaborators,
+  creatorId,
+  currentUserType,
+}: ShareDocumentDialogProps) => {
+  const user = useSelf();
 
-  const user = useSelf()
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const [open, setOpen] = useState(false)
-  const[loading , setLoading] = useState(false)
+  const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState<UserType>("viewer");
 
-  const [email, setEmail] = useState("")
-  const [userType , setUserType] = useState("")
-
-  const shareDocumentHandler = async () => {}
+  const shareDocumentHandler = async () => {};
 
   return (
-    <div>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button className="cursor-pointer bg-gradient-to-t from-blue-500 to-blue-400 flex h-9 gap-1 px-4 "
+        disabled= {currentUserType !== 'editor'}
+        >
+          <Image
+          src={"/assets/icons/share.svg"}
+          alt="share"
+          height={20}
+          width={20}
+          className="min-w-4 md:size-5"
+          />
+          Share
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-    </div>
-  )
-}
-
-export default ShareModal
+export default ShareModal;
